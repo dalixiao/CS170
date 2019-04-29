7405007d2953c3ce890550415280e1b46d4739fd
Dali Xiao <dalixiao@csil-19.cs.ucsb.edu>
2019-04-28 17:58:32 -0700

    test

--- pthread.c
+++ pthread.c
@@ -6,11 +6,12 @@
 #define exited -1
 #define Max_Threads 128
 
-struct TCB{
-    jmp_buf registers;
-    int* stackArea;
-    int threadStatus;
-};
+typedef struct{
+    pthread_t id;
+    jmp_but jb;
+    char *stack;
+}TCB;
+
 struct AllTCB{
     struct TCB ALLTCBs[Max_Threads];
     int Initialized;
@@ -20,8 +21,10 @@ struct AllTCB ThreadPool;
 
 int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void*), void *arg){
     static int Initialized = 0;
+    //initialize thread subsystem when pthread_create is called for the first time`
     if(Initialized == 0){
-        
+        Initialized = 1;
+             
     }
     
 
